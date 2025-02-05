2a486db7560d70ec4180a613231d9c7f447725dc
Jerry Britto <jbritto2004@gmail.com>
2025-02-05 16:15:32 +0530

    fixed changes in application

---
+++ __tests__ /server.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest");
+const app = require("../server.js");
+
+describe("API Endpoints", () => {
+  test("GET / should return 'Hello, World!'", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe("Hello, World! This is my Node.js app!");
+  });
+
+  test("GET /api should return JSON with a message", async () => {
+    const res = await request(app).get("/api");
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ message: "This is an API response!" });
+  });
+});
