74d41beb2ab1d961ef32bcc42626c2b8ebfe9677
Jerry Britto <jbritto2004@gmail.com>
2025-02-08 17:30:02 +0530

    forgot to change test file

--- __tests__ /server.test.js
+++ __tests__ /server.test.js
@@ -5,7 +5,7 @@ describe("API Endpoints", () => {
   test("GET / should return 'Hello, World!'", async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("Hello, World! This is my Node.js app!");
+    expect(res.text).toBe("Hello, World! This is my Node.js app! How was your day");
   });
 
   test("GET /api should return JSON with a message", async () => {
